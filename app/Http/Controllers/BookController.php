83a721b6af703546febb1abfbae84183b69a231e
handikoswara <39146228+handikoswara@users.noreply.github.com>
2021-01-01 15:25:09 +0700

    243

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,7 +13,8 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+        $books = \App\Models\Book::with('categories')->paginate(10);
+        return view('books.index', ['books' => $books]);
     }
 
     /**
@@ -78,7 +79,9 @@ class BookController extends Controller
      */
     public function edit($id)
     {
-        //
+        $book = \App\Models\Book::findOrFail($id);
+
+        return view('books.edit', ['book' => $book]);
     }
 
     /**
@@ -90,7 +93,31 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $book = \App\Models\Book::findOrFail($id);
+
+        $book->title = $request->get('title');
+        $book->slug = $request->get('slug');
+        $book->description = $request->get('description');
+        $book->author = $request->get('author');
+        $book->publisher = $request->get('publisher');
+        $book->stock = $request->get('stock');
+        $book->price = $request->get('price');
+        $new_cover = $request->file('cover');
+
+        if ($new_cover) {
+            if ($book->cover && file_exists(storage_path('app/public/' . $book->cover))) {
+                \Storage::delete('public/' . $book->cover);
+            }
+
+            $new_cover_path = $new_cover->store('book-covers', 'public');
+            $book->cover = $new_cover_path;
+        }
+
+        $book->updated_by = \Auth::user()->id;
+        $book->status = $request->get('status');
+        $book->save();
+        $book->categories()->sync($request->get('categories'));
+        return redirect()->route('books.edit', [$book->id])->with('status', 'Book successfully updated');
     }
 
     /**
@@ -101,6 +128,9 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $book = \App\Models\Book::findOrFail($id);
+        $book->delete();
+
+        return redirect()->route('books.index')->with('status', 'Book moved to trash');
     }
 }
